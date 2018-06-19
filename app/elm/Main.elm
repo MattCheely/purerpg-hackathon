f0d719bab7545b3385a85f50d282ad51416dd915
Matthew Cheely <matthew.cheely@genesys.com>
2018-06-18 22:10:30 -0400

    Move creature code to it's own module, and add an encoder & decoder

--- app/elm/Main.elm
+++ app/elm/Main.elm
@@ -1,5 +1,6 @@
 module Main exposing (main)
 
+import Creature exposing (Attack, Creature, CreatureType(..))
 import Html exposing (Html, button, div, img, text)
 import Html.Attributes exposing (class, src)
 import Html.Events exposing (onClick)
@@ -37,50 +38,6 @@ type alias CombatModel =
     }
 
 
-type alias Creature =
-    { creatureType : CreatureType
-    , hitPoints : Int
-    }
-
-
-type CreatureType
-    = Goblin
-    | Fighter
-    | Wizard
-
-
-newCreature : CreatureType -> Creature
-newCreature creatureType =
-    { creatureType = creatureType
-    , hitPoints = maxHp creatureType
-    }
-
-
-maxHp : CreatureType -> Int
-maxHp class =
-    case class of
-        Wizard ->
-            6
-
-        Fighter ->
-            10
-
-        Goblin ->
-            5
-
-
-type alias Attack =
-    { attacker : Creature
-    , victim : Creature
-    , result : AttackResult
-    }
-
-
-type AttackResult
-    = Hit Int
-    | Miss
-
-
 init : String -> ( Model, Cmd Msg )
 init token =
     ( { token = token, appModel = SelectingCharacter }
@@ -115,8 +72,8 @@ updateApp msg appModel =
     case ( appModel, msg ) of
         ( SelectingCharacter, CharacterSelected creatureType ) ->
             ( WithCharacter
-                { character = newCreature creatureType
-                , enemy = newCreature Goblin
+                { character = Creature.new creatureType
+                , enemy = Creature.new Goblin
                 , turnActions = []
                 }
             , Cmd.none
@@ -135,36 +92,16 @@ updateCombat msg model =
         PlayerAttack ->
             let
                 playerAttackResult =
-                    doAttack model.character model.enemy
+                    Creature.attack model.character model.enemy
 
                 enemyAttackResult =
-                    doAttack playerAttackResult.victim playerAttackResult.attacker
+                    Creature.attack playerAttackResult.victim playerAttackResult.attacker
             in
             { model
                 | character = enemyAttackResult.victim
                 , enemy = enemyAttackResult.attacker
                 , turnActions = [ playerAttackResult, enemyAttackResult ]
             }
-
-
-doAttack : Creature -> Creature -> Attack
-doAttack attacker victim =
-    let
-        damage =
-            case attacker.creatureType of
-                Wizard ->
-                    4
-
-                Fighter ->
-                    2
-
-                Goblin ->
-                    1
-    in
-    { attacker = attacker
-    , victim = { victim | hitPoints = victim.hitPoints - damage }
-    , result = Hit damage
-    }
 
 
 
@@ -197,41 +134,11 @@ combatView : CombatModel -> Html Msg
 combatView model =
     div []
         [ div []
-            [ creatureView model.character.creatureType ]
-        , div []
-            [ div [] [ text (toString model.character.creatureType ++ " hp: " ++ toString model.character.hitPoints) ]
-            , img [ src (creatureImg model.character.creatureType) ] []
-            , img [ src (creatureImg model.enemy.creatureType) ] []
-            , div [] [ text (toString model.enemy.creatureType ++ " hp: " ++ toString model.enemy.hitPoints) ]
+            [ Creature.showSprite model.character
+            , Creature.showSprite model.enemy
             ]
         , div [] [ button [ onClick (CombatEvent PlayerAttack) ] [ text "attack!" ] ]
         ]
-
-
-creatureView : CreatureType -> Html Msg
-creatureView creature =
-    case creature of
-        Wizard ->
-            text "You're a wizard, Harry!"
-
-        Fighter ->
-            text "Conan, what is best in life?!"
-
-        Goblin ->
-            text "Splork smash!"
-
-
-creatureImg : CreatureType -> String
-creatureImg class =
-    case class of
-        Wizard ->
-            "images/wizard.png"
-
-        Fighter ->
-            "images/fighter.png"
-
-        Goblin ->
-            "images/goblin.png"
 
 
 
